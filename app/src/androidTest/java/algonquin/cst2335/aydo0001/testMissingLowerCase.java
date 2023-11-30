12881d7009382959f7c3e8ef99e0c1d90594f052
aydoganali <a.aydogan@outlook.com>
2023-11-30 09:39:05 -0500

    Finished W10

--- app/src/androidTest/java/algonquin/cst2335/aydo0001/testMissingLowerCase.java
+++ app/src/androidTest/java/algonquin/cst2335/aydo0001/testMissingLowerCase.java
@@ -1,18 +1,10 @@
 package algonquin.cst2335.aydo0001;
 
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
 
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
@@ -36,16 +28,16 @@ public class testMissingLowerCase {
      */
     @Test
 
-    public void testMissingLowerCase() {
-        ViewInteraction appCompatEditText = onView(withId(R.id.passwordText));
-        appCompatEditText.perform(replaceText("PAASSWORD123"));
-
-        ViewInteraction materialButton = onView( withId(R.id.loginButton));
-        materialButton.perform(click());
-
-        ViewInteraction textView2 = onView(withId(R.id.textView2));
-        textView2.check(matches(withText("Your password needs upper and lower case")));
-    }
+//    public void testMissingLowerCase() {
+//        ViewInteraction appCompatEditText = onView(withId(R.id.passwordText));
+//        appCompatEditText.perform(replaceText("PAASSWORD123"));
+//
+//        ViewInteraction materialButton = onView( withId(R.id.loginButton));
+//        materialButton.perform(click());
+//
+//        ViewInteraction textView2 = onView(withId(R.id.textView2));
+//        textView2.check(matches(withText("Your password needs upper and lower case")));
+//    }
 
 
     private static Matcher<View> childAtPosition(
--- app/src/main/java/algonquin/cst2335/aydo0001/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/aydo0001/MainActivity.java
@@ -1,97 +1,84 @@
 package algonquin.cst2335.aydo0001;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import algonquin.cst2335.aydo0001.databinding.ActivityMainBinding;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-/**
- * This class is the starting point of the application
- * @author Ali Aydogan
- * @version 1.0
- */
+import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import algonquin.cst2335.aydo0001.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * Called when the activity is created.
-     *
-     * @param savedInstanceState A Bundle containing the saved state, if any.
-     */
     protected String cityName;
-    RequestQueue queue = null;
+    protected RequestQueue queue = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        //This part goes at the top of the onCreate function:
         queue = Volley.newRequestQueue(this);
-
-        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        ActivityMainBinding binding = ActivityMainBinding.inflate( getLayoutInflater() );
         setContentView(binding.getRoot());
 
         binding.getForecast.setOnClickListener(click -> {
-            cityName = binding.passwordText.getText().toString();
-            String stringURL = "";
-
+            cityName = binding.cityTextField.getText().toString();
+            String stringURL = null;
+            try {
+                stringURL = "https://api.openweathermap.org/data/2.5/weather?q=" + URLEncoder.encode(cityName, "UTF-8") + "&appid=b69c61857514fb35f81065ef7b77b61c&units=metric";
+            } catch (UnsupportedEncodingException e) {
+                throw new RuntimeException(e);
+            }
             //this goes in the button click handler:
             JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, stringURL, null,
                     (response) -> {
+                        try {
+                            JSONArray weather = response.getJSONArray("weather");
+                            JSONObject data = response.getJSONObject("main");
+                            binding.temperature.setText("The current temperature is " + data.getString("temp"));
+                            binding.maxTemp.setText("The max temperature is " + data.getString("temp_max"));
+                            binding.minTemp.setText("The min temperature is " + data.getString("temp_min"));
+                            binding.humidity.setText("The humidity is " + data.getString("humidity") + "%");
+                            binding.description.setText(weather.getJSONObject(0).getString("description"));
+                            String iconName = weather.getJSONObject(0).getString("icon");
+
+
+                            String imageUrl = "http://openweathermap.org/img/w/" + iconName + ".png";
+                            File f = new File(cityName);
+                            if (f.exists()) {
+                                binding.weatherImage.setImageBitmap(BitmapFactory.decodeFile(cityName));
+                            } else {
+                                ImageRequest imgReq = new ImageRequest(imageUrl, bitmap -> binding.weatherImage.setImageBitmap(bitmap),
+                                        1024, 1024, ImageView.ScaleType.CENTER, null, (error) -> {
+                                });
+                                queue.add(imgReq);
+                            }
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
                     },
-                    (error) -> {
-                    });
+                    (error) ->
+                    {
+                    }
+            );
+
             queue.add(request);
 
+
         });
-    }
-
-
-
-
-
-
-
-        binding.loginButton.setOnClickListener(click -> {
-
-            String userInput = binding.passwordText.getText().toString();
-
-            if (checkPasswordComplexity(userInput)) {
-                binding.textView2.setText("Your password is complex enough");
-            }
-            else binding.textView2.setText("Your password needs upper and lower case");
-        });
-
-
-
-
-    }
-
-    /** This function checks the complexity of the password string
-     *
-     * @param str The input string to be checked for password complexity.
-     * @return Returns true if str has upper and lower case,
-     * otherwise false
-     */
-    boolean checkPasswordComplexity (String str ) {
-
-        boolean foundUpperCase = false;
-        boolean foundLowerCase = false;
-
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if(Character.isLowerCase(c))
-                foundLowerCase = true;
-            else if (Character.isUpperCase(c))
-                foundUpperCase = true;
-
-
-        }
-        return foundLowerCase && foundUpperCase;
-    }
+   }
 }
